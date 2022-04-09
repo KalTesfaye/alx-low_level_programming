ea04b49f5d2318a183c0b9bf2f1d8b0fc1ad0f34
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-09 13:34:25 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <10; num++)
+	{
+		printf(nunm);
+	}
+	printf("\n")
+	return (0);
+}
+

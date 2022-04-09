6963470987e27f9b2b064ed54156d9871af4531b
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-09 12:57:41 -0700

    3-print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets;
+	char alphabetc;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	for (alphabetc = 'A'; alpnabetc <= 'Z'; alphabetc++)
+	{
+		putchar(alphabetc);
+	}
+	putchar('\n')
+	return (0);
+}

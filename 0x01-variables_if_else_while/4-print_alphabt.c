ef01bdca930c425aa80fcb39e5024563c477e250
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-09 13:22:39 -0700

    4 print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	char alphabet;
-	
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		if(alphabet == 'q' || alphabet == 'e')
+		if (alphabet == 'q' || alphabet == 'e')
 		{
 			continue;
 		}
 		putchar(alphabet);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }

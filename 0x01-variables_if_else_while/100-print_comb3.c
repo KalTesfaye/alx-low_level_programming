1503c3c85b72bf0eef9e55492bad8e407168e254
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-10 12:47:14 -0700

    100-print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int num2;
+
+	for (num2 = 48; num <= 57; num++)
+	{
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num2);
+		putchar(num);
+		if (num2 > num)
+		{
+			continue;
+		}
+		if (num == 57 && num2 == 56)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}

ac2fcfe41591d6bec82fc0ccb4bbf57bcbe8abbd
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-10 13:21:38 -0700

    101 print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,17 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num3 =48; num3 <= 57; num3++)
+	for (num3 = 48; num3 <= 57; num3++)
 	{
 	for (num2 = 48; num2 <= 57; num2++)
 	{
 	for (num = 48; num <= 57; num++)
 	{
-		if (num3 >=num2 && num2 >= num)
+		if (num3 >= num2 && num2 >= num)
 		{
 			continue;
 		}
+		putchar(num3);
 		putchar(num2);
 		putchar(num);
 		if (num == 57 && num2 == 56 && num3 == 55)

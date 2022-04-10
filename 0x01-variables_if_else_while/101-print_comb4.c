792d8c90f945b6a4be1bd5337a26f0dbb5bd6b65
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-10 13:17:54 -0700

    101 print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int num2;
+	int num3;
+
+	for (num3 =48; num3 <= 57; num3++)
+	{
+	for (num2 = 48; num2 <= 57; num2++)
+	{
+	for (num = 48; num <= 57; num++)
+	{
+		if (num3 >=num2 && num2 >= num)
+		{
+			continue;
+		}
+		putchar(num2);
+		putchar(num);
+		if (num == 57 && num2 == 56 && num3 == 55)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}

61a8ad2d5d4e10a87b01d236b794fc0e5805b294
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 12:10:34 -0700

    100-times tale

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *print_times_table - check code
+ *@n: vaue to be checked
+ *
+ * Return : 0
+ */
+void print_times_table(int n)
+{
+	int hour;
+	int minute;
+	int prod;
+
+	if (n < 15 && n > 0)
+	{
+	for (hour = 0; hour <= n; hour++)
+	{
+		for (minute = 0; minute <= n; minute++)
+		{
+			prod = hour * minute;
+			if (prod > 9)
+			{
+			_putchar(prod / 10 + '0');
+			}
+			if (minute != 0 && prod <= 9)
+			{
+				_putchar(' ');
+			}
+			_putchar(prod % 10 + '0');
+			if (minute == 9)
+			{
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+}

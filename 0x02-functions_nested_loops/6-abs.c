a4d9165c0d061d1ab6913511e633ca2e8acd2640
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 07:45:48 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check the code
+ * @n: the value to be checked
+ *
+ * Return: 0
+ */
+int  _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}

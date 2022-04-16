0f55550ba2955f8763dee4a4942890a868404f57
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 13:41:32 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int t1 = 0, t2 = 1;
+	int i;
+	long int nex = t1 + t2;
+	long int sum;
+
+	for (i = 1; i <= 50; i++)
+	{
+		if(nex % 2 == 0)
+		{
+			sum +=nex;
+		}
+
+		t1 = t2;
+		t2 = nex;
+		nex = t1 + t2;
+	}
+	printf("%d", sum);
+	printf("\n");
+	return (0);
+}

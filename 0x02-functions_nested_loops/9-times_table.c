5dabc27190a87d7f172f43a68a5eee5d9dde08f5
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 09:58:39 -0700

    9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,27 @@
 #include "main.h"
 
 /**
- * times_table - check code
+ * jack_bauer - check code
  *
  * Return : 0
  */
-void times_table(void)
+void jack_bauer(void)
 {
 	int hour;
 	int minute;
 
-	for (hour = 0; hour <= 9; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minute <= 9; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar(hour * minute + '00');
+			int prod;
+			prod = hour * minute;
+			_putchar(prod / 10 + '0');
+			_putchar(prod % 10 + '0');
 			if (minute == 9)
-			{
-				continue;
-			}
+					{
+					continue;
+					}
 			_putchar(',');
 			_putchar(' ');
 		}

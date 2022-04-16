a10ee377d6edfd3fed23bb5bdcebe2bb689cbd3f
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 10:10:35 -0700

    9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,18 @@ void times_table(void)
 {
 	int hour;
 	int minute;
+	int prod;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			int prod;
 			prod = hour * minute;
-			_putchar(prod / 10 + '0');
 			if (prod < 9)
 			{
-			_putchar(prod % 10 + '0');
+			_putchar(prod / 10 + '0');
 			}
+			_putchar(prod % 10 + '0');
 			if (minute == 9)
 					{
 					continue;

4ea630b641015a5c0b58bfd08c1bb8269bed60f5
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 11:41:50 -0700

    11-print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print upto 98
+ * @n: num to be checked
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int i;
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf(i);
+		}
+	}
+	else
+	{
+		for (i = 98; i < n; i++)
+		{
+			printf(i);
+		}
+	}
+	printf("\n")
+
+}

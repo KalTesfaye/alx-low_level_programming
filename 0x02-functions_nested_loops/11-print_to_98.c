d877c4c415df0bd1ea90ef21465278b9589cf2bc
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 11:53:49 -0700

    11-print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,23 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d",i);
+			if (i == 98)
+			{
+				continue;
+			}
+			printf(", ")
 		}
 	}
 	else
 	{
-		for (i = 98; i < n; i++)
+		for (i = n; i <= 98; i--)
 		{
 			printf("%d",i);
+			if (i == 98)
+			{
+				continue;
+			}
+			printf(", ")
 		}
 	}
 	printf("\n");

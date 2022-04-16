6e0b4d163a671d8f0992102239eb8e51a6c806fe
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 08:01:48 -0700

    7-print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: value to be checked
+ *
+ * return: 0
+ */
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n%10;
+	return (m);
+}

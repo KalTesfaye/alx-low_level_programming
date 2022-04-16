d4523baed7eee55242150d4925cab6415918b87a
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 13:05:19 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i = 1; i < 1024; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			mul += i;
 		}
 	}
-	printf("%d",mul);
+	printf("%d", mul);
 	printf("\n");
 	return (0);
 }

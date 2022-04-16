85bf4f78c9b035fd157bbed319250f4f6c0a7665
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-16 13:01:15 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include<stdio>
+/**
+ * main- entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int mul;
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if(i % 3 == 0 || i % 5 == 0)
+		{
+			mul += i;
+		}
+	}
+	printf("%d",mul);
+	printf("\n");
+	return (0);
+}

e4e4d8d6b27d79c9b2d03f5ec8bd7b038e3ee709
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-18 16:19:10 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy one string to other
+ * @dest: pointer to be copied
+ * @src: pointer to the first str
+ *
+ * Return: 0
+ */
+void *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		dest[j] = src[j];
+	}
+	
+}

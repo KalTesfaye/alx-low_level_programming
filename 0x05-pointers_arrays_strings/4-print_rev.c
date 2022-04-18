ab2865413506ae8f1fbd691e93c0ef00dcef94ba
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-18 14:35:05 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string int reverse
+ * @s: str to be print
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	char *m;
+	int c;
+	int i;
+
+	c = 0;
+	m = str;
+	while (m[c] != '\0')
+	{
+		c++;
+	}
+	for (i = c; i >= 0; i--)
+	{
+		_putchar(m[i])
+	}
+	_putchar('\n');
+}

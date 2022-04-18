5e058e5119b2d93b7c920d3ae02996ffe3ead5ec
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-18 13:12:25 -0700

    1-swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap-int - swap 2 values
+ * @a: first arg
+ * @b: second arg
+ *
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}

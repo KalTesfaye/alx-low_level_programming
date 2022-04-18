2022498f71aece7041c5d493268cd519d0af45e0
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-18 13:35:53 -0700

    1-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen -return the length of str
+ * @s: the str to be measure
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	c =0;
+	while (s[u] != '\0')
+	{
+		u++
+	}
+	return (u);
+}

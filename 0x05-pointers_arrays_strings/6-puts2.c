ae26381f88c22b681b664ea00237583a9be67a82
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-18 15:20:36 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: str to be print
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	char *m;
+	int c;
+
+	c = 0;
+	m = str;
+	while (m[c] != '\0')
+	{
+		if (c % 2 == 0)
+		{
+		_putchar(m[c]);
+		c++;
+		}
+	}
+	_putchar('\n');
+}

21f1f57936a58076c1d74c94b67714f2db5964fc
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-18 15:38:47 -0700

    7-puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - print a string int reverse
+ * @str: str to be print
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	char *m;
+	int c;
+	int i;
+
+	c = 0;
+	m = str;
+	while (m[c] != '\0')
+	{
+		c++;
+	}
+	for (i = (c - 1)/2; i <= 0; i++)
+	{
+		_putchar(m[i]);
+	}
+	_putchar('\n');
+}

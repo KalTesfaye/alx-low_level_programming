75f26ff35e358050ae8278aabb4d20a168a03203
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-18 15:14:54 -0700

    print rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,18 @@ void rev_string(char *s)
 	char *m;
 	int c;
 	int i;
+	int j;
 
 	c = 0;
 	m = s;
+	j = 0;
 	while (m[c] != '\0')
 	{
 		c++;
 	}
 	for (i = c - 1; i >= 0; i--)
 	{
-		s[i] = m[i];
+		s[j] = m[i];
+		j++;
 	}
 }

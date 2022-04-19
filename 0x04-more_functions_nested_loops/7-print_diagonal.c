32b75fff0aaad572aa43261c4c635ad2ca96dda3
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-19 15:40:02 -0700

    7-print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,12 +9,21 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < n; i++)
+	for (j =0; j < n; j++)
 	{
+	for (i = 0; i <= j; i++)
+	{
+		if (i == j)
+		{
 		_putchar('\\');
-		_putchar('\n');
-		_putchar(' ' );
+		}
+		else
+		{
+		_putchar(' ');
+		}
 	}
 	_putchar('\n');
+	}
 }

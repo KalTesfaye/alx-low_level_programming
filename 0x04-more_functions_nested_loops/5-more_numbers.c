d6a6fb0a0856533200cc1c57e157f4a9d5d1e36a
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-19 15:04:39 -0700

    5-more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,25 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int first_num;
+	int second_num;
 
-	for (j = 0; i <10; i++)
+	for (j = 0; i < 10; i++)
 	{
 	for (i = 0; i <= 14; i++)
 	{
-		_putchar(i + '0');
+		if (i < 10)
+		{
+			second_num = i;
+		}
+		else
+		{
+			first_num = i / 10;
+			second_num = i % 10;
+			_putchar(first_num + '0');
+		}
+		_putchar(second_num + '0');
 	}
 	_putchar('\n');
 	}
-	_putchar('\n');
 }

4e9c13aede4ba750b21e26f73aaf0d40227afad0
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-19 15:25:02 -0700

    7-print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  print_diagonal - print straight line
+ * @n: num of line
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = n; i > 0; i--)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}

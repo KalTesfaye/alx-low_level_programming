bbfd81584b73398580f303b7b0b2d11694c3fe6c
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-19 12:30:34 -0700

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - change string to number
+ * @s: character to be pass
+ *
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	unsigned int number,i;
+	int sign;
+
+	sign = 1;
+	number = 0;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (is_numerical(s[i]))
+		{
+			number = (s[i] - 48) + number * 10;
+		}
+		if (s[i + 1] == ' ')
+		{
+			break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	return (number * sign);
+}

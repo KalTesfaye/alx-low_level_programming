396a2d12e49e5cc17e4d3153d97a4f44af0234bb
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-19 14:49:57 -0700

    5-more functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 to 9
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; i <10; i++)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}

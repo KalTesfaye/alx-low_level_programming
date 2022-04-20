a310de43628711102b9e4314aa30c1c8835b18c2
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-20 11:49:19 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatinate two strings
+ * @dest: the destination string
+ * @src: source string 
+ *
+ * Return: Dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	return (*dest);
+}

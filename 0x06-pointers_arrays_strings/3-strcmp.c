42082c69bc512a08629fc6baf1b27696a2efb85c
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-21 13:57:29 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int *_strcmp(char *s1, *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\n' || s2[i] != '\n'; i++)
+	for (i = 0; s1[i] != '\n' &&  s2[i] != '\n'; i++)
 	{
 		if (s1[i] > s2[i])
 		{
-			return (-15);
+			return (15 * -1);
 		}
 		else if (s1[i] < s2[i])
 		{

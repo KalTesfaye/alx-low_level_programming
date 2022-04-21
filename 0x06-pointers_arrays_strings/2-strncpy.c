730003fe5207da0a30fcd83382f2e2a1ef065548
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-21 12:52:19 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - string that copyto dest
+ * @dest: destination string
+ * @src: source
+ * @n: lengthof char
+ *
+ * Return: Dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+	}
+	for  (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}

3b6fcef6cd3c0c7216c9428ca671c91ab3e8e2f5
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-21 13:54:13 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two chars
+ * @s1: the first char
+ * @s2: the second chars
+ *
+ * Return -ve +ve or 0
+ */
+int *_strcmp(char *s1, *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\n' || s2[i] != '\n'; i++)
+	{
+		if (s1[i] > s2[i])
+		{
+			return (-15);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (15);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (0);
+}

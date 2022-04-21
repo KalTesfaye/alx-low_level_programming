37145b32bf4a24b5a1231ac5a5e7c97c5c650bfd
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-21 13:07:59 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 	}
-	for  ( ; i < n; i++)
+	for  (j  =i ; j < n; j++)
 	{
-		dest[i] = '\0';
+		dest[j] = '\0';
 	}
 	return (dest);
 }

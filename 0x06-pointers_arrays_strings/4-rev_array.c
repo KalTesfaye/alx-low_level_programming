d3972eac3f9dbe1b7307d8c8d0e79b2aa0a1cb6b
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-22 09:40:13 -0700

    4-rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int rev[n];
+	int temp;
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		rev[n - 1 - i] = a[i];
+		temp = a[i];
+		a[i] = rev[n -1-i];
+		rev[n - 1 - i] = temp;
 	}
-	return (rev);
 }

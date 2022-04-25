6a2579abe97645380c99106263cb68c8e1b68cb7
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-25 12:37:17 -0700

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory constant size
+ * @s: memory location
+ * @b: constant byte 
+ * @n: number of memory area
+ *
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+		{
+		unsigned char *ptr = s;
+		while (n < 0)
+		{
+			*ptr++ = b;
+			n--;
+		}
+		return (s);
+		}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x07 even more arrays pointers and strings

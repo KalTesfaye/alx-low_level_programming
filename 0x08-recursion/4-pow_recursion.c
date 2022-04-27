944102c27e30460c91e1a98595cd45c11b6ac600
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-27 16:10:02 -0700

    4-pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the power of a given num
+ * @x: the given number
+ * @y: the power to be r
+ *
+ * Return: the power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	int pow;
+
+	if (y <= 0)
+	{
+		return(-1);
+	}
+	else
+	{
+		pow = x * _pow-recursion(x, y - 1);
+		return (pow);
+	}
+}

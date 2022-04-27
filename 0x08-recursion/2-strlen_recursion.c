bb06b0a2870b0f4d97ae3f59324eb60ca767cc0b
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-27 15:22:10 -0700

    2-strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the length of the string
+ * @s: the string to be measure
+ *
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*s != '\0')
+	{
+		return (i + _strlen_recursion(--s));
+	}
+}

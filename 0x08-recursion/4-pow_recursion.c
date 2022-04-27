246e34e78482b15df072fcd0f045e9b744619432
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-27 16:14:38 -0700

    4-pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,13 +11,13 @@ int _pow_recursion(int x, int y)
 {
 	int pow;
 
-	if (y <= 0)
-	{
-		return(-1);
-	}
-	else
+	if (y > 0)
 	{
 		pow = x * _pow_recursion(x, y - 1);
 		return (pow);
+	}
+	else
+	{
+		return (-1);
 	}
 }

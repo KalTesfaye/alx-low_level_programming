e0aa40637c95be72531e480db636ae9f023dc482
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-27 14:14:50 -0700

    0-puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print str with new line
+ * @s:the string to be printed
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
+}

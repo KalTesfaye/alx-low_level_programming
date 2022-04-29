3fae21b608d30ad67db2501d37d9a22c9af12eeb
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-29 14:32:15 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - the entry point
+ * @argc: count of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n",argv[0]);
+	return (0);
+}

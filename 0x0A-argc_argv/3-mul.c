8056d4e0e0a33fdb816f33bf7036624fe084bc8a
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-04-29 15:25:09 -0700

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - the entry point
+ * @argc: count of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+	if (argc == 3)
+		{
+			sum = atoi(argv[1]) * atoi(argv[2]);
+			printf ("%d\n", sum);
+			return (0);
+		}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}

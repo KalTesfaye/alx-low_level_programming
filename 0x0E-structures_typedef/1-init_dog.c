ba07214db9f0775aca51837496263bcc1e5af869
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-09 14:02:36 -0700

    1-init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *dd;
-
-	dd = d;
-	dd->name = name;
-	dd->age = age;
-	dd->owner = owner;
+	
+	d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }

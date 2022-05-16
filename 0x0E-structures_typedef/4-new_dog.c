714ca41a269c998caa07e2bd8359d4eaf4a6574c
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-16 11:30:48 -0700

    4-new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,41 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	
 	dog_t *d;
-	d = malloc(sizeof(struct dog));
-	
+	char *nam, *own;
+	int i = 0, lenn = 0, leno = 0;
+
+	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+	if (name != NULL && owner != NULL)
+	{
+		while (name[i++])
+			lenn++;
+		i = 0;
+		while (owner[i++])
+			leno++;
+		i = 0;
+	}
+	nam = malloc(sizeof(char) * (lenn + 1));
+	own = malloc(sizeof(char) * (leno + 1));
+	if (nam == NULL || own == NULL)
+		{
+			free(d), free(nam), free(own);
+			return(NULL);
+		}
+	while (name[i])
+	{
+		nam[i] == name[i];
+		i++;
+	}
+	nam[i] == name[i];
+	i = 0;
+	while (owner[i])
+	{
+		own[i] == owner[i];
+		i++;
+	}
+	own[i] == owner[i];
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

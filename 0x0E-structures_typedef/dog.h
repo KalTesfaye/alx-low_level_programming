fda8778e5f3918aee2b61f9b11da509da29beefd
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-16 11:44:11 -0700

    dog h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for the atribute dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
 struct dog
 {
 	char *name;
@@ -11,3 +20,4 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif

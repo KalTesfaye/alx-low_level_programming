a1caf60085cc8750dd0424f52ada14670eba4103
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-16 12:13:34 -0700

    2 print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	if (d-> age >= 0)
-		printf("Age: 0.6f\n", d->age);
+		printf("Age: %0.6f\n", d->age);
 	else
 		printf("Age: (nil)\n");
 	printf("Owner: %s\n", d->owner);

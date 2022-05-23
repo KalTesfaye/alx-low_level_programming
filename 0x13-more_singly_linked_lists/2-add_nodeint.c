b5c280fcb0cc88a71617417c0cc8dc9ed8aaa174
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-23 12:58:13 -0700

    2 add nodeint

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -19,12 +19,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	num = n;
-	if (num == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
 
 	new->n = num;
 	new->next = *head;

dcc526beb94d967e6bb8a85386ab4578ddfbcf9a
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-23 13:37:18 -0700

    4-free listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,7 +5,7 @@
  * free_listint - Frees a listint_t list.
  * @head: A pointer to the listint_t list.
  */
-void free_listint(listint_t *head);
+void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 

a4073a409fb34bba80f6910a9efea64f3700662a
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-26 11:38:06 -0700

    3-set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,6 +4,7 @@
  * set_bit - Set bit value to 1
  * @n: input
  * @index: index
+ *
  * Return: 0
  */
 int set_bit(unsigned long int *n, unsigned int index)
@@ -11,8 +12,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int b;
 
 	if (n == NULL)
+	{
 		return (-1);
-
+	}
 	b = 1 << index;
 
 	*n = *n | b;

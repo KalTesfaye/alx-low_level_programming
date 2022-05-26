4a5b2dfaf127af736226d0dc55b37ca8b0b65ef1
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-26 12:08:16 -0700

    4-clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * clear_bit - Entry Point
+:x
  * @n: input
  * @index: index
  * Return: 0
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)i
+{
 	unsigned long int b;
 
 	if (n == NULL)
 		return (-1);
-
 	b = 1 << index;
-
 	if ((b | *n) == *n)
 		*n = *n ^ b;
-
 	return (1);
 }

ea2e4bcfdbe854d69ed8f19f2e39d59c25c81471
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-05-26 11:23:04 -0700

    2-get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - Entry Point
+ * @n: input
+ * @index: index
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int b;
+
+	b = (n >> index) & 1;
+
+	if ((b == 1) || (b == 0))
+		return (b);
+	else
+		return (-1);
+}
